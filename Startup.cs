b0d958e37b0d1a48d28bd8468a2b528eb0fb7b0b
Rahul A <x00169650@mytudublin.ie>
2020-03-09 01:37:36 +0000

    generalized connection strings for multi environment

--- Startup.cs
+++ Startup.cs
@@ -37,12 +37,16 @@ namespace DotNetCoreSqlDb
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             // Use SQL Database if in Azure, otherwise, use SQLite
-            if(Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production")
-                services.AddDbContext<MyDatabaseContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("MyDbConnection")));
-            else
+
+            var envvar = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            
+            if(String.IsNullOrEmpty(envvar))
                 services.AddDbContext<MyDatabaseContext>(options =>
                     options.UseSqlite("Data Source=localdatabase.db"));
+            
+            else 
+                 services.AddDbContext<MyDatabaseContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("MyDbConnection")));
 
             // Automatically perform database migration
             services.BuildServiceProvider().GetService<MyDatabaseContext>().Database.Migrate();
